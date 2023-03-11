2de1dedb111135cbc17a5c82a98763e83810a4d7
Andrey Smirnov <andreysmirnov@me.com>
2023-03-11 15:00:14 +0300

    test db name added

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -75,7 +75,10 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'PORT': os.getenv('DB_PORT'),
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        },
     }
 }
 
